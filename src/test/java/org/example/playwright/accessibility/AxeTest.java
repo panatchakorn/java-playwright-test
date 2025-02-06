423ace9c792d0b3c53b2f770bd41065de6494cc4
Panatchakorn <panatchakorn@gmail.com>
2025-02-06 19:19:04 +1300

    Save Axe reports

--- src/test/java/org/example/playwright/accessibility/AxeTest.java
+++ src/test/java/org/example/playwright/accessibility/AxeTest.java
@@ -3,6 +3,7 @@ package org.example.playwright.accessibility;
 import com.deque.html.axecore.results.AxeResults;
 import org.example.playwright.toolshop.fixtures.AxeTestFixtures;
 import org.example.playwright.toolshop.login.LoginPage;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,27 +13,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AxeTest extends AxeTestFixtures {
 
+    private AxeResults accessibilityResults;
+    private String testName;
+
     @DisplayName("Homepage")
     @Test
     void homepageAxeTest() {
+        testName = "Homepage";
         page.navigate("https://practicesoftwaretesting.com/");
-        AxeResults accessibilityResults = makeAxeBuilder()
+        accessibilityResults = makeAxeBuilder()
 //                .include("#specific-element-under-test")
                 .analyze();
-        printResults(accessibilityResults, "Homepage");
         assertEquals(Collections.emptyList(), accessibilityResults.getViolations());
     }
 
     @DisplayName("Login Page")
     @Test
     void loginPageAxeTest() {
+        testName = "Login Page";
         LoginPage loginPage = new LoginPage(page);
         loginPage.open();
-        AxeResults accessibilityResults = makeAxeBuilder()
+        accessibilityResults = makeAxeBuilder()
 //                .include("#specific-element-under-test")
                 .analyze();
-        printResults(accessibilityResults, "Login Page");
         assertEquals(Collections.emptyList(), accessibilityResults.getViolations());
+    }
+
+    @AfterEach
+    void afterEachTest() {
+        printResults(accessibilityResults, testName);
+        createReport(accessibilityResults, testName);
     }
 
 }
--- src/test/java/org/example/playwright/toolshop/fixtures/AxeTestFixtures.java
+++ src/test/java/org/example/playwright/toolshop/fixtures/AxeTestFixtures.java
@@ -1,8 +1,15 @@
 package org.example.playwright.toolshop.fixtures;
 
 import com.deque.html.axecore.playwright.AxeBuilder;
+import com.deque.html.axecore.playwright.Reporter;
 import com.deque.html.axecore.results.AxeResults;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 // https://www.deque.com/axe/core-documentation/api-documentation/#axe-core-tags
@@ -10,24 +17,51 @@ import java.util.Arrays;
 public class AxeTestFixtures extends PlaywrightTestCase {
     protected AxeBuilder makeAxeBuilder() {
         return new AxeBuilder(page)
-                .withTags(Arrays.asList(new String[]{"wcag2a", "wcag2aa", "wcag2aaa", "wcag21a", "wcag21aa", "wcag22aa", "best-practice", "ACT"}))
+                .withTags(Arrays.asList("wcag2a", "wcag2aa", "wcag2aaa", "wcag21a", "wcag21aa", "wcag22aa", "best-practice", "ACT"))
                 .exclude("#comonly-released-element-with-known-issues");
     }
 
     public void printResults(AxeResults accessibilityResults, String testName) {
         System.out.println("Test Name: " + testName);
         System.out.println("Current Page URL: " + page.url());
-        System.out.println("Accessibility VIOLATIONS count: " + accessibilityResults.getViolations().size());
+        System.out.println("Accessibility VIOLATIONS count: " + accessibilityResults.getViolations()
+                .size());
         System.out.println("Accessibility violations: " + accessibilityResults.getViolations());
         System.out.println("------------------------------------");
-        System.out.println("Accessibility PASSES count: " + accessibilityResults.getPasses().size());
+        System.out.println("Accessibility PASSES count: " + accessibilityResults.getPasses()
+                .size());
         System.out.println("Accessibility passes: " + accessibilityResults.getPasses());
         System.out.println("------------------------------------");
-        System.out.println("Accessibility INCOMPLETE count: " + accessibilityResults.getIncomplete().size());
+        System.out.println("Accessibility INCOMPLETE count: " + accessibilityResults.getIncomplete()
+                .size());
         System.out.println("Accessibility incomplete: " + accessibilityResults.getIncomplete());
         System.out.println("------------------------------------");
-        System.out.println("Accessibility INAPPLICABLE count: " + accessibilityResults.getInapplicable().size());
+        System.out.println("Accessibility INAPPLICABLE count: " + accessibilityResults.getInapplicable()
+                .size());
         System.out.println("Accessibility inapplicable: " + accessibilityResults.getInapplicable());
+    }
+
+    public void createReport(AxeResults accessibilityResults, String testName) {
+        testName = testName.trim().replace(" ", "_").toLowerCase();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMyyyy_HHmmssSSS");
+        String timestamp = LocalDateTime.now()
+                .format(formatter);
+        String fileName = testName + "_" + timestamp + ".json";
+
+        try {
+            Path reportDir = Paths.get("target", "reports", "axe");
+            if (!Files.exists(reportDir)) {
+                Files.createDirectories(reportDir);
+            }
+
+            Path filePath = reportDir.resolve(fileName);
+            Files.createFile(filePath);
+            Reporter reporter = new Reporter();
+            reporter.JSONStringify(accessibilityResults, filePath.toAbsolutePath()
+                    .toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
